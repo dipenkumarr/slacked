28584cf8d4fb7147a1a849ef0de550e9847f905b
Dipen Kumar <dipenbachani@gmail.com>
2025-07-21 19:50:29 -0700

    add UI blocks

--- notification-service/slack-notification.go
+++ notification-service/slack-notification.go
@@ -30,4 +30,25 @@ func main() {
 	} else {
 		buildResult = ":x: " + buildResult
 	}
+
+	dividerSection1 := slack.NewDividerBlock()
+	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
+	preTextField := slack.NewTextBlockObject("mrkdwn", preText+"\n\n", false, false)
+	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
+
+	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
+	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
+
+	msg := slack.MsgOptionBlocks(preTextSection, dividerSection1, jenkinsBuildDetailsSection)
+
+	_, _, _, err = api.SendMessage(
+		"C09650BR7SR",
+		msg,
+	)
+	if err != nil {
+		fmt.Printf("failed posting message: %v\n", err)
+		return
+	}
+	fmt.Printf("Message successfully sent to channel\n")
+
 }
