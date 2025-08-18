444aedf565f54e5108928145da2ff44043b09b52
Dipen Kumar <dipenbachani@gmail.com>
2025-08-17 21:31:42 -0700

    Refactor HandleRequest to process SQS events

--- notification-service/slack-notification.go
+++ notification-service/slack-notification.go
@@ -45,14 +45,15 @@ func getSecret(ctx context.Context, secretName string) (string, error) {
 	return *result.SecretString, nil
 }
 
-func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func HandleRequest(ctx context.Context, sqsEvent events.SQSEvent) error {
 
 	// 1. Get the secrets from Secrets Manager.
 	secretName := os.Getenv("SECRET_NAME")
 	secretJSON, err := getSecret(ctx, secretName)
 	if err != nil {
-		fmt.Printf("failed to get secret: %v\n", err)
-		return events.APIGatewayProxyResponse{Body: "Internal Server Error", StatusCode: 500}, err
+		fmt.Printf("FATAL: failed to get secret: %v\n", err)
+		// Return an error to send the entire batch back to the queue for a retry
+		return err
 	}
 	fmt.Println("successfully retrieved secret from Secrets Manager")
 
@@ -60,57 +61,63 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 
 	err = json.Unmarshal([]byte(secretJSON), &credentials)
 	if err != nil {
-		fmt.Printf("failed to unmarshal secret JSON: %v\n", err)
-		return events.APIGatewayProxyResponse{Body: "Internal Server Error", StatusCode: 500}, err
+		fmt.Printf("FATAL: failed to unmarshal secret JSON: %v\n", err)
+		return err
 	}
 
-	// 2. Decode the JSON payload from the request body
-	build := JenkinsBuild{}
-
-	err = json.Unmarshal([]byte(request.Body), &build)
-	if err != nil {
-		fmt.Printf("could not unmarshal request body: %s\n", err)
-		return events.APIGatewayProxyResponse{Body: "Bad Request", StatusCode: 400}, err
-	}
-
-	// 3. Create a Slack client using the credentials
+	// 2. Create a Slack client using the credentials
 	api := slack.New(credentials.SlackBotToken)
 
-	// 4. Prepare the message to be sent to Slack
-	preText := "*Hello! Your Jenkins build has completed!*"
-	jenkinsURL := "*Build URL:* " + build.BuildURL
-	buildResult := "*" + build.BuildResult + "*"
-	buildNumber := "*" + fmt.Sprint(build.BuildNumber) + "*"
-	jobName := "*" + build.JobName + "*"
+	// 3. Process each message in the batch
+	for _, message := range sqsEvent.Records {
+		fmt.Printf("Processing message ID: %s\n", message.MessageId)
 
-	if buildResult == "*SUCCESS*" {
-		buildResult = ":white_check_mark: " + buildResult
-	} else {
-		buildResult = ":x: " + buildResult
+		// 3a. Decode the JSON payload from the SQS message body.
+		build := JenkinsBuild{}
+		err := json.Unmarshal([]byte(message.Body), &build)
+		if err != nil {
+			fmt.Printf("ERROR: could not unmarshal message body for messageId %s: %v\n", message.MessageId, err)
+			// Log the error and continue with the next message
+			continue
+		}
+
+		// 3b. Prepare the message to be sent to Slack
+		preText := "*Hello! Your Jenkins build has completed!*"
+		jenkinsURL := "*Build URL:* " + build.BuildURL
+		buildResult := "*" + build.BuildResult + "*"
+		buildNumber := "*" + fmt.Sprint(build.BuildNumber) + "*"
+		jobName := "*" + build.JobName + "*"
+
+		if buildResult == "*SUCCESS*" {
+			buildResult = ":white_check_mark: " + buildResult
+		} else {
+			buildResult = ":x: " + buildResult
+		}
+
+		dividerSection1 := slack.NewDividerBlock()
+		jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
+		preTextField := slack.NewTextBlockObject("mrkdwn", preText+"\n\n", false, false)
+		jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
+		jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
+		preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
+		msg := slack.MsgOptionBlocks(preTextSection, dividerSection1, jenkinsBuildDetailsSection)
+
+		// 3c. Send the message
+		_, _, _, err = api.SendMessage(
+			credentials.SlackChannelID,
+			msg,
+		)
+		if err != nil {
+			// If sending to Slack fails, this is a transient error, so retry.
+			fmt.Printf("ERROR: failed to send Slack message for messageId %s: %v\n", message.MessageId, err)
+			return err
+		}
+
+		fmt.Printf("Successfully processed and sent notification for message ID: %s\n", message.MessageId)
 	}
 
-	dividerSection1 := slack.NewDividerBlock()
-	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
-	preTextField := slack.NewTextBlockObject("mrkdwn", preText+"\n\n", false, false)
-	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
-
-	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
-	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
-
-	msg := slack.MsgOptionBlocks(preTextSection, dividerSection1, jenkinsBuildDetailsSection)
-
-	// 5. Send the message
-	_, _, _, err = api.SendMessage(
-		credentials.SlackChannelID,
-		msg,
-	)
-	if err != nil {
-		fmt.Printf("Error sending message: %s\n", err)
-		return events.APIGatewayProxyResponse{Body: "Error sending Slack message", StatusCode: 500}, err
-	}
-
-	// 6. Return a success response to API Gateway
-	return events.APIGatewayProxyResponse{Body: "Message sent successfully!", StatusCode: 200}, nil
+	// 6. Return a nil on success, indicating all messages were processed.
+	return nil
 
 }
 
